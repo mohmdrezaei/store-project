111e2b13db2fe7bbe76ec9dca1efb713097d98af
mohmdrezaei <mhwmdrz9@gmail.com>
2024-09-28 00:41:52 +0330

    Create search section

--- src/pages/ProductsPage.jsx
+++ src/pages/ProductsPage.jsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+import { ImSearch } from "react-icons/im";
 import Card from "../components/card/Card";
 import Loader from "../components/loader/Loader";
 import { useProducts } from "../context/ProductContext";
@@ -6,17 +8,31 @@ import styles from "./ProductsPage.module.css";
 
 function ProductsPage() {
   const products = useProducts();
+  const [search, setSearch] = useState("");
 
   return (
-    <div className={styles.container}>
-      <div className={styles.products}>
-        {!products.length && <Loader />}
-        {products.map((product) => (
-          <Card key={product.id} data={product} />
-        ))}
+    <>
+      <div>
+        <input
+          type="text"
+          placeholder="Search..."
+          value={search}
+          onChange={(e) => setSearch(e.target.value.toLowerCase().trim())}
+        />
+        <button>
+          <ImSearch />
+        </button>
       </div>
-      <div>sidebar</div>
-    </div>
+      <div className={styles.container}>
+        <div className={styles.products}>
+          {!products.length && <Loader />}
+          {products.map((product) => (
+            <Card key={product.id} data={product} />
+          ))}
+        </div>
+        <div>sidebar</div>
+      </div>
+    </>
   );
 }
 
