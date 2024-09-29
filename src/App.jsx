83621d3d1d0ab789493e2a31800696e99c6677c1
mohmdrezaei <mhwmdrz9@gmail.com>
2024-09-29 23:19:44 +0330

    Combination of Context and useReducer

--- src/App.jsx
+++ src/App.jsx
@@ -5,18 +5,21 @@ import DetailsPage from "./pages/DetailsPage";
 import CheckoutPage from "./pages/CheckoutPage";
 import PageNotFound from "./pages/PageNotFound";
 import ProductsProvider from "./context/ProductContext";
+import CartProvider from "./context/CartContext";
 
 function App() {
   return (
-    <ProductsProvider>
-      <Routes>
-        <Route path="/" element={<Navigate to="/products" replace />} />
-        <Route path="/products" element={<ProductsPage />} />
-        <Route path="/products/:id" element={<DetailsPage />} />
-        <Route path="/checkout" element={<CheckoutPage />} />
-        <Route path="*" element={<PageNotFound />} />
-      </Routes>
-    </ProductsProvider>
+    <CartProvider>
+      <ProductsProvider>
+        <Routes>
+          <Route path="/" element={<Navigate to="/products" replace />} />
+          <Route path="/products" element={<ProductsPage />} />
+          <Route path="/products/:id" element={<DetailsPage />} />
+          <Route path="/checkout" element={<CheckoutPage />} />
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
+      </ProductsProvider>
+    </CartProvider>
   );
 }
 
