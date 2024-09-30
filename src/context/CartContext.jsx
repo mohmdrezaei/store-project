9ad70d84ea15b6ab6d0b95021b77e8986822b0ef
mohmdrezaei <mhwmdrz9@gmail.com>
2024-09-30 13:58:29 +0330

    Implement reducer function

--- src/context/CartContext.jsx
+++ src/context/CartContext.jsx
@@ -1,9 +1,28 @@
 import { createContext, useContext, useReducer } from "react";
+import { sumProducts } from "../helpers/helper";
 
-const initialState = () => {};
+const initialState = {
+  selectedItems: [],
+  itemsCounter: 0,
+  total: 0,
+  checkout: false,
+};
 
 const reducer = (state, action) => {
-    console.log(action)
+  console.log(action);
+  switch (action.type) {
+    case "ADD_ITEM":
+      if (!state.selectedItems.find((item) => item.id === action.payload.id)) {
+        state.selectedItems.push({ ...action.payload, quantity: 1 });
+      }
+      return {
+        ...state,
+        checkout: false,
+        ...sumProducts(state.selectedItems),
+      };
+    default:
+      throw new Error("Invalid action");
+  }
 };
 
 const CartContext = createContext();
@@ -17,9 +36,9 @@ function CartProvider({ children }) {
 }
 
 const useCart = () => {
-    const {state , dispatch} = useContext(CartContext)
-    return [state , dispatch]
+  const { state, dispatch } = useContext(CartContext);
+  return [state, dispatch];
 };
 
 export default CartProvider;
-export {useCart}
+export { useCart };
--- src/helpers/helper.js
+++ src/helpers/helper.js
@@ -36,10 +36,20 @@ const getInitialQuery = (searchParams) => {
   if (search) query.search = search;
   return query;
 };
+
+const sumProducts = (products) => {
+  const itemsCounter = products.reduce((acc, cur) => acc + cur.quantity, 0);
+  const total = products
+    .reduce((acc, cur) => acc + cur.price * cur.quantity, 0)
+    .toFixed(2);
+
+    return { itemsCounter , total}
+};
 export {
   shortenText,
   searchProducts,
   filterProducts,
   createQueryObject,
   getInitialQuery,
+  sumProducts,
 };
