4745cfcfb325c37b1ada709d1937e0ff5b5072b5
mohmdrezaei <mhwmdrz9@gmail.com>
2024-09-30 18:59:05 +0330

    Adding other actions to reducer

--- src/context/CartContext.jsx
+++ src/context/CartContext.jsx
@@ -20,6 +20,38 @@ const reducer = (state, action) => {
         checkout: false,
         ...sumProducts(state.selectedItems),
       };
+    case "REMOVE_ITEM":
+      const newSelectedItems = state.selectedItems.filter(
+        (item) => item.id !== action.payload.id
+      );
+      return {
+        ...state,
+        selectedItems: [...newSelectedItems],
+        ...sumProducts(newSelectedItems),
+      };
+    case "INCRESE":
+      const increaseIndex = state.selectedItems.findIndex(
+        (item) => item.id === action.payload.id
+      );
+      state.selectedItems[increaseIndex].quantity++;
+      return {
+        ...state , 
+       ...sumProducts(state.selectedItems)
+      }
+    case "DEACRESE" :
+      const decreaseIndex = state.selectedItems.findIndex(item=> item.id === action.payload.id)
+      state.selectedItems[decreaseIndex].quantity--;
+      return {
+        ...state,
+        ...sumProducts(state.selectedItems)
+      }
+      case "CHECKOUT" :
+        return {
+          selectedItems : [],
+          itemsCounter: 0 ,
+          total : 0,
+          checkout : true
+        }
     default:
       throw new Error("Invalid action");
   }
